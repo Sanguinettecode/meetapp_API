6693b3f0d3695c124213351c127b6ee94c173207
Andre Sanguinette <sanguinettecode@gmail.com>
2019-09-03 12:43:45 -0300

    adição de configuração e envio de emails a partir de uma inscrição com uso de view tamplates

--- src/app/controllers/RegisterController.js
+++ src/app/controllers/RegisterController.js
@@ -1,12 +1,20 @@
 import { isBefore } from 'date-fns';
 import Meetup from '../models/Meetup';
+import User from '../models/User';
 import Registration from '../models/Registration';
+import Mail from '../../lib/Mail';
 
 class RegisterController {
   async store(req, res) {
     const { meetupID } = req.params;
 
-    const meetup = await Meetup.findByPk(meetupID);
+    const meetup = await Meetup.findByPk(meetupID, {
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['name', 'email'],
+      },
+    });
     if (!meetup) {
       return res.status(400).json({ error: 'no meetup has been founded' });
     }
@@ -32,19 +40,31 @@ class RegisterController {
     ) {
       return res.status(401).json({ error: 'you aready registered' });
     }
-    if (
-      userRegistration.some(
-        async regist => (await regist.meetup.datetime) === meetup.datetime
-      )
-    ) {
-      return res
-        .status(401)
-        .json({ error: 'You are registered on meetup with same date' });
-    }
+    // if (
+    //   userRegistration.some(
+    //     async regist => (await regist.meetup.datetime) === meetup.datetime
+    //   )
+    // ) {
+    //   return res
+    //     .status(401)
+    //     .json({ error: 'You are registered on meetup with same date' });
+    // }
     const newRegistration = await Registration.create({
       meetup_id: meetup.id,
       organizer_id: meetup.user_id,
       user_id: req.userId,
+    });
+    const user = await User.findByPk(req.userId);
+
+    await Mail.sendmail({
+      to: `${meetup.user.name} <${meetup.user.email}>`,
+      subject: '[Meetapp] Nova inscrição no meetup',
+      template: 'cancelation',
+      context: {
+        provider: meetup.user.name,
+        user: user.name,
+        date: meetup.datetime,
+      },
     });
     return res.json(newRegistration);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,10 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2de676781e1eb0',
+    pass: '049b9eb3e13a8e',
+  },
+  default: 'Equipe Meetup <oi@meetup.com>',
+};
